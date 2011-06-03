dd2015d4d861683669f4fd4bca9f1e73f4e31138
Kazuhiro Osawa <yappo@shibuya.pl>
2011-06-03 17:57:03 +0900

    initial commit

---
+++ lib/Acme/SEGV.pm
@@ -0,0 +1,32 @@
+package Acme::SEGV;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::SEGV -
+
+=head1 SYNOPSIS
+
+    use Acme::SEGV;
+
+=head1 DESCRIPTION
+
+Acme::SEGV is
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
